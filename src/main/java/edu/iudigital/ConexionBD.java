a0ef47444050fe6902f874be3ab8845af82da8c0
marlon <marlon.monterrosa.m@gmail.com>
2026-03-07 22:29:18 -0500

    me falto corredir la clave, importar las librerias y retoques

--- src/main/java/edu/iudigital/ConexionBD.java
+++ src/main/java/edu/iudigital/ConexionBD.java
@@ -1,4 +1,20 @@
 package edu.iudigital;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class ConexionBD {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/autos_colombia";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection conectar() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+            return null;
+        }
+    }
 }
--- src/main/java/edu/iudigital/RegistroDAO.java
+++ src/main/java/edu/iudigital/RegistroDAO.java
@@ -1,6 +1,6 @@
 package edu.iudigital;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.time.LocalDateTime;
 
 public class RegistroDAO {
 
